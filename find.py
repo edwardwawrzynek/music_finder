f42fa9e5f116b0919065fafd3b69496b25c2e287
Edward Wawrzynek <edward@wawrzynek.com>
2024-01-20 23:27:31 -0700

    first commit

---
+++ find.py
@@ -0,0 +1,161 @@
+#! /usr/bin/env python3
+
+import subprocess
+import os
+import re
+import discogs_client
+import music_tag
+import argparse
+from yt_dlp import YoutubeDL
+
+d_client = discogs_client.Client('MusicFinder/0.1', user_token='qcaPDIVYeLReeGdsiXlKzmBlvuaEJEWGivecoQeT')
+
+# tag all files in the current directory
+def tag(artist, album, year, artwork):
+    # walk all the files in the directory
+    files = [file for file in os.listdir() if file.endswith("m4a")]
+    num_files = len(files)
+
+    for file in files:
+        # get the index of the track in the album
+        file_index = int(file.split('-')[0])
+        # get the rest of the album title from the file name
+        file_title = '-'.join(file.removesuffix('.m4a').split('-')[1:])
+
+        # remove common suffixes from title)
+        file_title = re.sub(r'(\s*\((Official|Live|Lyric|Music|Demo|Audio|feat|Closed-|((19|20)\d\d))[^\)]*\)\s*)+', "", file_title)
+        
+        # remove artist from beginning of title (if present)
+        file_title = re.sub(r'\s*' + artist + '\s*-\s*', '', file_title)
+
+        # Tag file
+        f = music_tag.load_file(file)
+
+        f['title'] = file_title
+        f['album'] = album
+        f['artist'] = artist
+        f['year'] = year
+        f['totaltracks'] = num_files
+        f['tracknumber'] = file_index
+        f['artwork'] = artwork
+
+        print("Tagging: " + file_title + ", " + album + ", " + artist + ", " + str(year) + ", " + str(file_index) + "/" + str(num_files))
+
+        f.save()
+
+# Find an album by name and artist
+def find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt):
+    results = d_client.search(album, type='release', artist=artist)
+    if results.pages == 0:
+        print("ERROR: Cannot find album")
+        exit(1)
+    
+    res = results[0]
+
+    album_artist = res.artists[0].name
+    album_title = res.title
+    year = res.year
+    img_url = res.images[0]['uri']
+
+    print()
+    print("Found release: " + album_title + ", " + album_artist + ", " + str(year))
+
+    if not skip_prompt:
+        proceed = input("Proceed [Y/n]? ")
+
+        if proceed.startswith("n") or proceed.startswith("N"):
+            print("STOPPING")
+            exit(0)
+    
+    if not skip_dir_setup:
+        # create directory for artist and album
+        artist_path = music_dir + "/" + artist.replace(" ", "_")
+
+        if not os.path.exists(artist_path):
+            os.makedirs(artist_path)
+        os.chdir(artist_path)
+
+        if not os.path.exists(album):
+            os.makedirs(album)
+        os.chdir(album)
+    
+    download(yt_playlist)
+    
+    # download artwork
+    path = "COVER_ART.png"
+    download_res = subprocess.run(['wget', '-O', path, img_url]).returncode
+
+    if download_res != 0:
+        print("ERROR: Cannot download album artwork")
+        exit(1)
+    
+    with open(path, 'rb') as img_file:
+        tag(album_artist, album_title, year, img_file.read())
+    
+    os.remove(path)
+
+# download the given youtube playlist
+def download(yt_playlist):
+    # download
+    result = subprocess.run(["yt-dlp", "-o", "%(playlist_index)s-%(title)s.%(ext)s", "-f", "139", "https://www.youtube.com/watch?list=" + yt_playlist]).returncode
+
+    if result != 0:
+        print("ERROR: download failed")
+        exit(result)
+
+# Identify the artist and album from a youtube playlist
+def extract_album(yt_playlist):
+    # get video info from ydl
+    ydl = YoutubeDL({'playlist_items': '1'})
+    info = ydl.extract_info('https://www.youtube.com/watch?list=' + yt_playlist, download=False)
+    info = ydl.sanitize_info(info)
+    # extract album artist & title
+    title = info['title']
+    artist = info['entries'][0]['uploader']
+    return title, artist
+
+
+def run(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt):
+    find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt)
+
+parser = argparse.ArgumentParser(prog='music_find', description='Find Music')
+parser.add_argument('yt_playlist')
+parser.add_argument('--album')
+parser.add_argument('--artist')
+parser.add_argument('--use_current_dir', action='store_true')
+parser.add_argument('--music_dir', default='/home/edward/Music/')
+parser.add_argument('--yes', action='store_true')
+
+args = parser.parse_args()
+
+# if full url was given, extract playlist
+if "list=" in args.yt_playlist:
+    args.yt_playlist = args.yt_playlist.split("list=")[1]
+
+# if album or artist was not given, try to get it from album
+if args.artist == None or args.album == None:
+    e_album, e_artist = extract_album(args.yt_playlist)
+
+    if args.album == None:
+        if not args.yes:
+            print()
+            use_album = input("Use album: " + e_album + " [Y/n]? ")
+            if use_album.startswith("n") or use_album.startswith("N"):
+                args.album = input("Album name: ")
+            else:
+                args.album = e_album
+        else:        
+            args.album = e_album
+
+    if args.artist == None:
+        if not args.yes:
+            print()
+            use_artist = input("Use artist: " + e_artist + " [Y/n]? ")
+            if use_artist.startswith("n") or use_artist.startswith("N"):
+                args.artist = input("Artist name: ")
+            else:
+                args.artist = e_artist
+        else:    
+            args.artist = e_artist
+
+run(args.yt_playlist, args.album, args.artist, args.music_dir, args.use_current_dir, args.yes)
