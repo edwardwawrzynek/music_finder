f9aa900278c2d8e8b3e29f92e42166001c8b8d88
Edward Wawrzynek <edward@wawrzynek.com>
2025-03-26 22:35:20 -0600

    misc

--- find.py
+++ find.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import subprocess
+import time
 import os
 import re
 import discogs_client
@@ -57,7 +58,7 @@ def tag(artist, album, year, artwork):
         f.save()
 
 # Find an album by name and artist
-def find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt):
+def find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt, vfmt):
     results = d_client.search(album, type='release', artist=artist)
     if results.pages == 0 or len(results) == 0:
         print("ERROR: Cannot find album")
@@ -70,14 +71,18 @@ def find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_promp
     year = res.year
     img_url = res.images[0]['uri']
 
-
     # Search for just CD releases. If we find one with the same album, artist, and year as the top result, use it's art instead.
     # Discogg tends to have better album art for CD releases
     results_cd = d_client.search(album, type='release', artist=artist, format='CD')
     if len(results_cd) > 0:
+        count = 0
         for res_cd in results_cd:
+            count += 1
             if res_cd.artists[0].name.lower() == album_artist.lower() and res_cd.title.lower() == album_title.lower() and res_cd.year == year and res_cd.images and len(res_cd.images) > 0:
                 img_url = res_cd.images[0]['uri']
+                break
+            
+            if count > 30:
                 break
 
     print()
@@ -102,7 +107,7 @@ def find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_promp
             os.makedirs(album)
         os.chdir(album)
     
-    download(yt_playlist)
+    download(yt_playlist, vfmt)
     
     # download artwork
     path = "COVER_ART.png"
@@ -117,14 +122,29 @@ def find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_promp
     
     os.remove(path)
 
+# youtube download format
+YT_FORMAT = "249"
+
 # download the given youtube playlist
-def download(yt_playlist):
+def download(yt_playlist, vfmt):
     # download
-    result = subprocess.run(["yt-dlp", "-o", "%(playlist_index)s-%(title)s.%(ext)s", "-f", "139", "https://www.youtube.com/watch?list=" + yt_playlist]).returncode
+    result = subprocess.run(["yt-dlp", "-o", "%(playlist_index)s-%(title)s.%(ext)s", "-f", vfmt, "https://www.youtube.com/watch?list=" + yt_playlist]).returncode
 
     if result != 0:
         print("ERROR: download failed")
         exit(result)
+    
+    # convert any webm to m4a
+    files = next(os.walk('.'), (None, None, []))[2]
+    for file in files:
+        if file.endswith(".webm") or file.endswith(".mp4"):
+            result = subprocess.run(["ffmpeg", "-i", file, "-b:a", "48k", "-vn", file.removesuffix(".webm") + ".m4a"]).returncode
+
+            if result != 0:
+                print("ERROR: webm -> m4a conversion failed")
+                exit(result)
+            # remove webm file
+            os.remove(file)
 
 # Identify the artist and album from a youtube playlist
 def extract_album(yt_playlist):
@@ -138,8 +158,8 @@ def extract_album(yt_playlist):
     return title, artist
 
 
-def run(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt):
-    find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt)
+def run(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt, vfmt):
+    find_album(yt_playlist, album, artist, music_dir, skip_dir_setup, skip_prompt, vfmt)
 
 parser = argparse.ArgumentParser(prog='music_find', description='Find Music')
 parser.add_argument('yt_playlist')
@@ -148,6 +168,7 @@ parser.add_argument('--artist')
 parser.add_argument('--use_current_dir', action='store_true')
 parser.add_argument('--music_dir', default='/home/edward/Music/')
 parser.add_argument('--yes', action='store_true')
+parser.add_argument('--yt_fmt', default=YT_FORMAT)
 
 args = parser.parse_args()
 
@@ -181,4 +202,4 @@ if args.artist == None or args.album == None:
         else:    
             args.artist = e_artist
 
-run(args.yt_playlist, args.album, args.artist, args.music_dir, args.use_current_dir, args.yes)
+run(args.yt_playlist, args.album, args.artist, args.music_dir, args.use_current_dir, args.yes, args.yt_fmt)
